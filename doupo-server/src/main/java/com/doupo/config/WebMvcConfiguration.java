fcc8025ee3568ee112897e8212753cf1e7a22be8
Siborne <904389425@qq.com>
2026-02-11 00:26:51 +0800

    docs(api): 更新项目接口文档标题为斗破快送
    - 将管理端接口文档标题从苍穹外卖更改为斗破快送
    - 将用户端接口文档标题从苍穹外卖更改为斗破快送
    - 更新接口文档描述信息为斗破快送项目
    - 保持接口版本号为2.0不变

--- doupo-server/src/main/java/com/doupo/config/WebMvcConfiguration.java
+++ doupo-server/src/main/java/com/doupo/config/WebMvcConfiguration.java
@@ -57,9 +57,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket docket1() {
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("斗破快送项目接口文档")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("斗破快送项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .groupName("管理端接口")
@@ -74,9 +74,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket docket2() {
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("斗破快送项目接口文档")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("斗破快送项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .groupName("用户端")
